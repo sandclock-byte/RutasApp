2a3d9988ffd86e1f71780fa9db68073d73343c9a
Fernando Bernal <fernando.bernal.diaz@gmail.com>
2022-08-15 20:55:31 -0500

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+
+const App = () => {
+  return (
+    <View>
+      <Text>App</Text>
+      <Icon name="infinite" size={50} />
+    </View>
+  )
+}
+
+export default App;
+
+const styles = StyleSheet.create({
+
+});

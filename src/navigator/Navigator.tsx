60dc3ec13330159d7a34bdcded15a151c63b37a3
Fernando Bernal <fernando.bernal.diaz@gmail.com>
2022-08-16 21:54:28 -0500

    Se condiciona para mostrar pantalla de inicio

--- src/navigator/Navigator.tsx
+++ src/navigator/Navigator.tsx
@@ -1,22 +1,36 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { MapScreen } from '../screens/MapScreen';
 import { PermissionsScreen } from '../screens/PermissionsScreen';
+import { PermissionsContext } from '../context/PermissionsContext';
+import { LoadingScreen } from '../screens/LoadingScreen';
 
 const Stack = createNativeStackNavigator();
 
 export const Navigator = () => {
+
+    const { permissions } = useContext(PermissionsContext);
+
+    if (permissions.locationStatus === 'unavailable') {
+        return (
+            <LoadingScreen />
+        );
+    }
+
     return (
         <Stack.Navigator
-        screenOptions={{
-            headerShown: false,
-            contentStyle: {
-                backgroundColor: 'white'
-            }
-        }}
+            screenOptions={{
+                headerShown: false,
+                contentStyle: {
+                    backgroundColor: 'white'
+                }
+            }}
         >
-            <Stack.Screen name='PermissionsScreen' component={PermissionsScreen} />
-            <Stack.Screen name='MapScreen' component={MapScreen} />
+            {
+                (permissions.locationStatus === 'granted')
+                    ? <Stack.Screen name="MapScreen" component={MapScreen} />
+                    : <Stack.Screen name="PermissionsScreen" component={PermissionsScreen} />
+            }
         </Stack.Navigator>
     )
 }
--- src/screens/LoadingScreen.tsx
+++ src/screens/LoadingScreen.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet, ActivityIndicator } from 'react-native';
 
 export const LoadingScreen = () => {
   return (
-      <View>
-          <Text>LoadingScreen</Text>
+      <View style={{
+          flex: 1,
+          justifyContent: 'center',
+          alignItems: 'center',
+          backgroundColor: 'white',
+      }}>
+        <ActivityIndicator size={50} color='black' />
       </View>
   )
 }

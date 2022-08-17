0d846fff8a8dfeaad625c3bfa11544a9df82396c
Fernando Bernal <fernando.bernal.diaz@gmail.com>
2022-08-16 21:05:51 -0500

    Se otorga permiso

--- src/screens/PermissionsScreen.tsx
+++ src/screens/PermissionsScreen.tsx
@@ -1,15 +1,45 @@
 import React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet, Button, Platform } from 'react-native';
+import { check, PERMISSIONS, PermissionStatus, request } from 'react-native-permissions';
 
 export const PermissionsScreen = () => {
+
+  const checkLocationPermission = async () => {
+
+    let permissionStatus: PermissionStatus;
+
+    if (Platform.OS === 'ios') {
+      // iOS
+      // permissionStatus = await check(PERMISSIONS.IOS.LOCATION_WHEN_IN_USE)
+      permissionStatus = await request(PERMISSIONS.IOS.LOCATION_WHEN_IN_USE)
+    } else {
+      // Android
+      // permissionStatus = await check(PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION)
+      permissionStatus = await request(PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION)
+    }
+
+    console.log({ permissionStatus });
+
+  }
+
   return (
-      <View>
-          <Text>PermissionsScreen</Text>
-      </View>
+    <View style={styles.container}>
+      <Text style={{ color: 'black' }}>PermissionsScreen</Text>
+      <Button
+        title='Permiso'
+        onPress={checkLocationPermission}
+      />
+    </View>
   )
 }
 
 
 const styles = StyleSheet.create({
+
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
 
 });

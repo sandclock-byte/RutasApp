e29015d631b7c9d1b5db62fdb5dc349822140d3a
Fernando Bernal <fernando.bernal.diaz@gmail.com>
2022-08-19 21:29:29 -0500

    Se hace configuracion de mapas en Android

--- src/screens/MapScreen.tsx
+++ src/screens/MapScreen.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import { Text, View, StyleSheet } from 'react-native';
 
+import MapView from 'react-native-maps';
+
 export const MapScreen = () => {
   return (
-      <View>
-          <Text style={{color: 'black'}}>MapScreen</Text>
-      </View>
+    <View style={{ flex: 1 }}>
+      <MapView
+        style={{ flex: 1 }}
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      />
+    </View>
   )
 }
 
